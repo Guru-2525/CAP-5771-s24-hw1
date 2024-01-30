1fe463b690d3122a6b1d593d721e8aa8f7a9ce24
GURUPRASAD REDDY <gpreddy2526@gmail.com>
2024-01-29 20:58:38 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,15 +1,27 @@
 def question6_1():
-    """
+    
     answer = {
-        'equal_width': ['string', integer],
-        'equal_frequency': ['string', integer]
+        'equal_width': ['no change', 9],
+        'equal_frequency': ['no change', 9]
     }
-    """
+    
     return answer
 
 def question6_2():
+
+    answer = {
+        'equal_width': ['change', 9],
+        'equal_frequency': ['no change', 9]
+    }
+    
     return answer
 
 def question6_3():
+
+    answer = {
+        'equal_width': ['change', 4],
+        'equal_frequency': ['change', 4]
+    }
+    
     return answer 
 
